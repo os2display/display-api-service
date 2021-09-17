7f1e50e7455a80e777820395c792f8501491736c
Jesper Kristensen <cableman@linuxdev.dk>
2021-09-17 13:32:04 +0200

    AR-583: Added screen groups endpoint

---
+++ src/DataTransformer/ScreenGroupOutputDataTransformer.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataTransformer;
+
+use ApiPlatform\Core\DataTransformer\DataTransformerInterface;
+use App\Dto\ScreenGroup as ScreenGroupDTO;
+use App\Entity\ScreenGroup;
+
+class ScreenGroupOutputDataTransformer implements DataTransformerInterface
+{
+    public function transform($screenGroup, string $to, array $context = [])
+    {
+        /** @var ScreenGroup $screenGroup */
+        $output = new ScreenGroupDTO();
+        $output->title = $screenGroup->getTitle();
+        $output->description = $screenGroup->getDescription();
+        $output->modified = $screenGroup->getUpdatedAt();
+        $output->created = $screenGroup->getCreatedAt();
+        $output->modifiedBy = $screenGroup->getModifiedBy();
+        $output->createdBy = $screenGroup->getCreatedBy();
+
+        return $output;
+    }
+
+    public function supportsTransformation($data, string $to, array $context = []): bool
+    {
+        return ScreenGroupDTO::class === $to && $data instanceof ScreenGroup;
+    }
+
+}
---
+++ src/Dto/ScreenGroup.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Dto;
+
+class ScreenGroup
+{
+    public string $title = '';
+    public string $description = '';
+    public \DateTimeInterface $created;
+    public \DateTimeInterface $modified;
+    public string $modifiedBy = '';
+    public string $createdBy = '';
+}
