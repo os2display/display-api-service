3296dad4fa9bbd2fe03091f5ee13d4139127aa04
Jesper Kristensen <cableman@linuxdev.dk>
2021-09-17 12:51:33 +0200

    AR-583: Remove sub-resource paths for layout regions

---
+++ src/Api/OpenApiFactory.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Api;
+
+use ApiPlatform\Core\OpenApi\Factory\OpenApiFactoryInterface;
+use ApiPlatform\Core\OpenApi\OpenApi;
+use ApiPlatform\Core\OpenApi\Model;
+
+class OpenApiFactory implements OpenApiFactoryInterface
+{
+    private OpenApiFactoryInterface $decorated;
+
+    public function __construct(OpenApiFactoryInterface $decorated)
+    {
+        $this->decorated = $decorated;
+    }
+
+    public function __invoke(array $context = []): OpenApi
+    {
+        $openApi = $this->decorated->__invoke($context);
+
+        $paths = $openApi->getPaths()->getPaths();
+
+        // Remove sub-resource with these paths.
+        $exclude = ['/v1/screen-layout-regions/{ulid}', '/v1/screen-layout-regions'];
+
+        $filteredPaths = new Model\Paths();
+        foreach ($paths as $path => $pathItem) {
+            if (in_array($path, $exclude)) {
+                continue;
+            }
+            $filteredPaths->addPath($path, $pathItem);
+        }
+
+        return $openApi->withPaths($filteredPaths);
+    }
+}
