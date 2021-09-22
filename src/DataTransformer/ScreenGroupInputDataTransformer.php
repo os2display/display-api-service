e1b136ac5346370372e1d0fdd00f03b3ce1901fb
Jesper Kristensen <cableman@linuxdev.dk>
2021-09-22 15:28:08 +0200

    AR-583: Added post,put and delete to screen groups

---
+++ src/DataTransformer/ScreenGroupInputDataTransformer.php
@@ -0,0 +1,54 @@
+<?php
+
+// src/DataTransformer/BookInputDataTransformer.php
+
+namespace App\DataTransformer;
+
+use ApiPlatform\Core\DataTransformer\DataTransformerInterface;
+use ApiPlatform\Core\Serializer\AbstractItemNormalizer;
+use ApiPlatform\Core\Validator\ValidatorInterface;
+use App\Dto\ScreenGroupInput;
+use App\Entity\ScreenGroup;
+
+final class ScreenGroupInputDataTransformer implements DataTransformerInterface
+{
+    private ValidatorInterface $validator;
+
+    public function __construct(ValidatorInterface $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function transform($data, string $to, array $context = []): ScreenGroup
+    {
+        /* @var ScreenGroupInput $data */
+        $this->validator->validate($data);
+
+        $screenGroup = new ScreenGroup();
+        if (array_key_exists(AbstractItemNormalizer::OBJECT_TO_POPULATE, $context)) {
+            $screenGroup = $context[AbstractItemNormalizer::OBJECT_TO_POPULATE];
+        }
+
+        empty($data->title) ?: $screenGroup->setTitle($data->title);
+        empty($data->description) ?: $screenGroup->setDescription($data->title);
+        empty($data->createdBy) ?: $screenGroup->setCreatedBy($data->title);
+        empty($data->modifiedBy) ?: $screenGroup->setModifiedBy($data->title);
+
+        return $screenGroup;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsTransformation($data, string $to, array $context = []): bool
+    {
+        if ($data instanceof ScreenGroup) {
+            return false;
+        }
+
+        return ScreenGroup::class === $to && null !== ($context['input']['class'] ?? null);
+    }
+}
---
+++ src/Dto/ScreenGroupInput.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Dto;
+
+class ScreenGroupInput
+{
+    public string $title = '';
+    public string $description = '';
+    public string $modifiedBy = '';
+    public string $createdBy = '';
+}
