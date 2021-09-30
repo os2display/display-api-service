b9e868092646483a911c729f0fd480fa09c6afd3
Ture Gjørup <ture@ignatz.dk>
2021-09-30 14:01:28 +0200

    AR-543: Added RRule doctrine type

---
+++ src/DBAL/RRuleType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\DBAL;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+use RRule\RRule;
+
+class RRuleType extends Type
+{
+    public const RRULE = 'rrule';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform)
+    {
+        return $platform->getVarcharTypeDeclarationSQL($column);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform): ?RRule
+    {
+        if (null === $value) {
+            return null;
+        }
+
+        return new RRule($value);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if (null === $value) {
+            return null;
+        }
+
+        /* @var RRule $value */
+        return $value->rfcString(true);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getName(): string
+    {
+        return self::RRULE;
+    }
+}
