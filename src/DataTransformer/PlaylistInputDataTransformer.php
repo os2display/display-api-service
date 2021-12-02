55d66b0f9d671e3bc4c21a1cae33409ac21c7e97
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2021-12-02 12:43:17 +0100

    AR-692: Moved remove playlists into empty check

--- src/DataTransformer/PlaylistInputDataTransformer.php
+++ src/DataTransformer/PlaylistInputDataTransformer.php
@@ -32,13 +32,13 @@ final class PlaylistInputDataTransformer implements DataTransformerInterface
         empty($data->title) ?: $playlist->setTitle($data->title);
         empty($data->description) ?: $playlist->setDescription($data->description);
 
-        // Remove all schedules.
-        foreach ($playlist->getSchedules() as $schedule) {
-            $playlist->removeSchedule($schedule);
-        }
-
-        // Add schedules.
         if (!empty($data->schedules)) {
+            // Remove all schedules.
+            foreach ($playlist->getSchedules() as $schedule) {
+                $playlist->removeSchedule($schedule);
+            }
+
+            // Add schedules.
             foreach ($data->schedules as $scheduleData) {
                 $schedule = new Schedule();
                 $rrule = $this->utils->validateRRule($this->transformRRuleNewline($scheduleData['rrule']));
