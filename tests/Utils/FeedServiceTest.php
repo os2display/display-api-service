33e3cad428235b67221f710b83eea051d4f82752
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2021-12-15 14:39:15 +0100

    AR-685: Added FeedService tests

--- tests/Utils/FeedServiceTest.php
+++ tests/Utils/FeedServiceTest.php
@@ -2,25 +2,39 @@
 
 namespace App\Tests\Utils;
 
-use ApiPlatform\Core\Exception\InvalidArgumentException;
 use App\Feed\RssFeedType;
+use App\Repository\FeedRepository;
 use App\Service\FeedService;
-use App\Utils\IriHelperUtils;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class FeedServiceTest extends KernelTestCase
 {
     private FeedService $feedService;
+    private FeedRepository $feedRepository;
 
     protected function setUp(): void
     {
         $this::bootKernel();
         $this->feedService = static::getContainer()->get(FeedService::class);
+        $this->feedRepository = static::getContainer()->get(FeedRepository::class);
     }
 
     public function testGetFeedTypes(): void
     {
         $feedTypes = $this->feedService->getFeedTypes();
         $this->assertEquals(RssFeedType::class, $feedTypes[0]);
+    }
+
+    public function testGetFeedUrl(): void
+    {
+        $feed = $this->feedRepository->findOneBy([]);
+        $this->assertMatchesRegularExpression('@^/v\d/\w+/([A-Za-z0-9]{26})/data$@', $this->feedService->getFeedUrl($feed));
+    }
+
+    public function testGetData(): void
+    {
+        // @TODO: Test this.
+        // $feed = $this->feedRepository->findOneBy([]);
+        // $data = $this->feedService->getFeedUrl($feed);
     }
 }
