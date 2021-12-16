cbcd5dec93d311dccaf046722ea294b4f91b7ae1
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2021-12-16 13:03:33 +0100

    AR-685: Added caching to feed service

--- src/Service/FeedService.php
+++ src/Service/FeedService.php
@@ -5,11 +5,13 @@ namespace App\Service;
 use ApiPlatform\Core\Api\OperationType;
 use ApiPlatform\Core\Bridge\Symfony\Routing\RouteNameGenerator;
 use App\Entity\Feed;
+use Psr\Cache\CacheItemInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class FeedService
 {
-    public function __construct(private iterable $feedTypes, private UrlGeneratorInterface $urlGenerator)
+    public function __construct(private iterable $feedTypes, private CacheInterface $cache, private UrlGeneratorInterface $urlGenerator)
     {
     }
 
@@ -26,25 +28,49 @@ class FeedService
 
     public function getFeedUrl(Feed $feed): string
     {
-        // @TODO: Find solution without depending on @internal RouteNameGenerator.
-        $routeName = RouteNameGenerator::generate('getFeedData', 'feed', OperationType::ITEM);
+        // @TODO: Find solution without depending on @internal RouteNameGenerator for generating route name.
+        $routeName = RouteNameGenerator::generate('feed_data', 'Feed', OperationType::ITEM);
 
         return $this->urlGenerator->generate($routeName, ['id' => $feed->getId()]);
     }
 
+    public function getFeedConfiguration(Feed $feed): array
+    {
+        $feedSourceConfiguration = $feed->getFeedSource()->getConfiguration();
+        $feedConfiguration = $feed->getConfiguration();
+
+        return array_merge($feedSourceConfiguration, $feedConfiguration);
+    }
+
     public function getData(Feed $feed): ?array
     {
-        // @TODO: Check for cached result.
+        /** @var CacheItemInterface $cacheItem */
+        $cacheItem = $this->cache->getItem($feed->getId()->jsonSerialize());
 
-        $feedSource = $feed->getFeedSource();
-        $feedTypeClassName = $feedSource->getFeedType();
+        if ($cacheItem->isHit()) {
+            return $cacheItem->get();
+        } else {
+            $feedSource = $feed->getFeedSource();
+            $feedTypeClassName = $feedSource->getFeedType();
+            $feedConfiguration = $this->getFeedConfiguration($feed);
 
-        foreach ($this->feedTypes as $feedType) {
-            if ($feedType::class === $feedTypeClassName) {
-                return $feedType->getData($feedSource, $feed);
+            foreach ($this->feedTypes as $feedType) {
+                if ($feedType::class === $feedTypeClassName) {
+                    $data = $feedType->getData($feedSource, $feed);
+
+                    $cacheItem->set($data);
+
+                    if (isset($feedConfiguration['cache_expire'])) {
+                        $cacheItem->expiresAfter($feedConfiguration['cache_expire']);
+                    }
+
+                    $this->cache->save($cacheItem);
+
+                    return $data;
+                }
             }
-        }
 
-        return null;
+            return null;
+        }
     }
 }
