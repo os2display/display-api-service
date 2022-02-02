d4d7a556fd1fd78061fb9c9685b9f2faf577f434
Ture Gjørup <ture@ignatz.dk>
2022-02-02 10:54:50 +0100

    AR-542: Coding standards

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -5,7 +5,6 @@ namespace App\Repository;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
