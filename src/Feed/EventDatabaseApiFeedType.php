2ef4eb8d811783742f2751c435c6490a69d14031
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2022-03-04 15:34:44 +0100

    AR-769: Fixed merge issue

--- src/Feed/EventDatabaseApiFeedType.php
+++ src/Feed/EventDatabaseApiFeedType.php
@@ -2,8 +2,8 @@
 
 namespace App\Feed;
 
-use App\Entity\Feed;
-use App\Entity\FeedSource;
+use App\Entity\Tenant\Feed;
+use App\Entity\Tenant\FeedSource;
 use App\Service\FeedService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
