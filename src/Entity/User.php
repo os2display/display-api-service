b5eb1307ce9526dc2ea2e333a5b19d6df9b9b2cd
Ture Gjørup <ture@ignatz.dk>
2022-03-09 10:57:38 +0100

    AR-700: CS fixes

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,7 +6,6 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use JetBrains\PhpStorm\Internal\TentativeType;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
