dfd3bd565954a20151c56e1e086ae408b8c27369
Ture Gjørup <ture@ignatz.dk>
2022-03-09 10:54:45 +0100

    AR-700: Added user data to token endpoints

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,6 +6,7 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use JetBrains\PhpStorm\Internal\TentativeType;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -13,7 +14,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  */
-class User extends AbstractBaseEntity implements UserInterface, PasswordAuthenticatedUserInterface
+class User extends AbstractBaseEntity implements UserInterface, PasswordAuthenticatedUserInterface, \JsonSerializable
 {
     /**
      * @ORM\Column(type="string", length=180, unique=true)
@@ -279,5 +280,13 @@ class User extends AbstractBaseEntity implements UserInterface, PasswordAuthenti
         $this->provider = $provider;
 
         return $this;
+    }
+
+    public function jsonSerialize()
+    {
+        return [
+            'fullname' => $this->getFullName(),
+            'email' => $this->getEmail(),
+        ];
     }
 }
