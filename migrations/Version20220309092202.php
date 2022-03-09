8c593d1215eba2034e310b959f3ef6b4b1b52675
Ture Gjørup <ture@ignatz.dk>
2022-03-09 10:23:26 +0100

    Updated actions to mariadb 10.5

--- migrations/Version20220309092202.php
+++ migrations/Version20220309092202.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220301153248 extends AbstractMigration
+final class Version20220309092202 extends AbstractMigration
 {
     public function getDescription(): string
     {
