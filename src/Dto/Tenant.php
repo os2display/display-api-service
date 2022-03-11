d6af98144da32766a3ba4136c830cf1861ea950a
Sine <sinje@aarhus.dk>
2022-03-11 12:33:33 +0100

    AR-544: added tenant endpoint

---
+++ src/Dto/Tenant.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Dto;
+
+class Tenant
+{
+    public string $title = '';
+    public string $description = '';
+    public string $tenantKey = '';
+}
---
+++ tests/Api/GetTenantTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Tests\Api;
+
+use App\Entity\Template;
+use App\Entity\Tenant\FeedSource;
+use App\Entity\Tenant\Slide;
+use App\Entity\Tenant\Theme;
+use App\Tests\AbstractBaseApiTestCase;
+
+class GetTenantTest extends AbstractBaseApiTestCase
+{
+    public function testGetCollection(): void
+    {
+        $response = $this->getAuthenticatedClient()->request('GET', '/v1/tenants?itemsPerPage=10', ['headers' => ['Content-Type' => 'application/ld+json']]);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        $this->assertJsonContains([
+            '@context' => '/contexts/Tenant',
+            '@id' => '/v1/tenants',
+            '@type' => 'hydra:Collection',
+            'hydra:totalItems' => 2,
+
+        ]);
+
+        $this->assertCount(2, $response->toArray()['hydra:member']);
+    }
+
+}
