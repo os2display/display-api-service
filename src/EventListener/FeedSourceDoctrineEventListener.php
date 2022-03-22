a7896ba4a75f9d97187997dd6bd53b4a484e3430
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2022-03-22 11:10:59 +0100

    AR-806: Remove feed cache entry when feed or feed source is updated or removed.

---
+++ src/EventListener/FeedSourceDoctrineEventListener.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\Tenant\FeedSource;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Contracts\Cache\CacheInterface;
+
+class FeedSourceDoctrineEventListener
+{
+    public function __construct(private CacheInterface $feedsCache)
+    {
+    }
+
+    public function preRemove(FeedSource $feedSource, LifecycleEventArgs $event): void
+    {
+        // On feed source remove clear each feed cache entry.
+        $this->clearFeedData($feedSource);
+    }
+
+    public function postUpdate(FeedSource $feedSource, LifecycleEventArgs $event): void
+    {
+        // On feed source update clear each feed cache entry.
+        $this->clearFeedData($feedSource);
+    }
+
+    private function clearFeedData(FeedSource $feedSource): void {
+        $feeds = $feedSource->getFeeds();
+
+        foreach ($feeds as $feed) {
+            $this->feedsCache->delete($feed->getId()->jsonSerialize());
+        }
+    }
+}
