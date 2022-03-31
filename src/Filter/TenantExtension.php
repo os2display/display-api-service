b60319cc37617770b5a50d4d78ffa65f89e29c2e
Sine <sinje@aarhus.dk>
2022-03-31 13:37:35 +0200

    AR-544: remove filter add new filter

---
+++ src/Filter/TenantExtension.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Filter;
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\Security\Core\Security;
+
+final class TenantExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
+{
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null): void
+    {
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+
+    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, string $operationName = null, array $context = []): void
+    {
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+
+    private function addWhere(QueryBuilder $queryBuilder, string $resourceClass): void
+    {
+        if (null === $user = $this->security->getUser()) {
+            return;
+        }
+        $tenant= $user->getActiveTenant();
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+        //$queryBuilder->andWhere(sprintf('%s.tenant = :tenant', $rootAlias))
+        //->setParameter('tenant', $tenant);
+
+      }
+}
+
