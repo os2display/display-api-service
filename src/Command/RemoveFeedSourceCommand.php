91a90713a40e3151c2c7598ac40ff5fea346663d
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2022-04-20 09:44:02 +0200

    AR-821: Applied coding standards

--- src/Command/RemoveFeedSourceCommand.php
+++ src/Command/RemoveFeedSourceCommand.php
@@ -2,19 +2,14 @@
 
 namespace App\Command;
 
-use App\Entity\Tenant;
-use App\Entity\Tenant\FeedSource;
 use App\Repository\FeedRepository;
 use App\Repository\FeedSourceRepository;
-use App\Repository\TenantRepository;
-use App\Service\FeedService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
@@ -62,7 +57,7 @@ class RemoveFeedSourceCommand extends Command
         $feedSource = $this->feedSourceRepository->find($ulid);
 
         if (!$feedSource) {
-            $io->error("Feed source could not be found. Aborting.");
+            $io->error('Feed source could not be found. Aborting.');
 
             return Command::INVALID;
         }
