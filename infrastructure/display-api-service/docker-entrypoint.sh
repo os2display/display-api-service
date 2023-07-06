d656b83f5b74a42411289f45e156eee04978a2bf
Ture Gjørup <ture@ignatz.dk>
2023-07-06 11:51:09 +0200

    DISPLAY-986: Review feedback

--- infrastructure/display-api-service/docker-entrypoint.sh
+++ infrastructure/display-api-service/docker-entrypoint.sh
@@ -2,14 +2,6 @@
 
 set -eux
 
-## Start the PHP FPM process.
-echo "Starting PHP 8.1 FPM"
-
-# first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
-  set -- php-fpm "$@"
-fi
-
 ## Run templates with configuration.
 /usr/local/bin/confd --onetime --backend env --confdir /etc/confd
 
@@ -18,5 +10,13 @@ composer dump-env prod
 
 ## Warm-up Symfony cache (with the current configuration).
 /var/www/html/bin/console --env=prod cache:warmup
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+  set -- php-fpm "$@"
+fi
+
+## Start the PHP FPM process.
+echo "Starting PHP 8.1 FPM"
 
 exec php-fpm "$@"
