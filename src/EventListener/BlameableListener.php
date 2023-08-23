789d4c81ac58788834d2f35d68cdd5a271929360
jekuaitk <jekua@aarhus.dk>
2023-08-23 15:47:58 +0200

    DISPLAY-1007: Updated deprecated event subscriber

---
+++ src/EventListener/BlameableListener.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\Interfaces\BlameableInterface;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
+use Doctrine\ORM\Events;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Component\Security\Core\Security;
+
+#[AsDoctrineListener(event: Events::prePersist)]
+#[AsDoctrineListener(event: Events::preUpdate)]
+class BlameableListener
+{
+    public function __construct(
+        private Security $security
+    ) {}
+
+    public function prePersist(LifecycleEventArgs $args): void
+    {
+        $entity = $args->getObject();
+
+        if ($entity instanceof BlameableInterface) {
+            $user = $this->security->getUser();
+
+            if (null !== $user) {
+                $entity->setCreatedBy($user->getUserIdentifier());
+                $entity->setModifiedBy($user->getUserIdentifier());
+            }
+        }
+    }
+
+    public function preUpdate(LifecycleEventArgs $args): void
+    {
+        $entity = $args->getObject();
+
+        if ($entity instanceof BlameableInterface) {
+            $user = $this->security->getUser();
+
+            if (null !== $user) {
+                $entity->setModifiedBy($user->getUserIdentifier());
+            }
+        }
+    }
+}
