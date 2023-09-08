bafc40c2c3b97f861cbb48e212e327cfa91bb0a4
jekuaitk <jekua@aarhus.dk>
2023-09-08 15:25:25 +0200

    DISPLAY-1007: Added Vich preUpload listener

--- src/Security/TenantScope/PrePersistListener.php
+++ src/Security/TenantScope/PrePersistListener.php
@@ -2,50 +2,64 @@
 
 namespace App\Security\TenantScope;
 
+
 use App\Entity\Interfaces\TenantScopedEntityInterface;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
+use Doctrine\ORM\Event\PrePersistEventArgs;
 use Doctrine\ORM\Events;
-use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\Security\Core\Security;
+use Vich\UploaderBundle\Event\Event;
+use Vich\UploaderBundle\Event\Events as VichEvents;
+
 
 /**
- * PrePersistSubscriber Class.
+ * PrePersistListener Class.
  *
- * Doctrine lifecycle event subscriber to set tenant of all new
- * entities from the Users active tenant.
+ * Doctrine event listener to set tenant of all new
+ *  entities from the Users active tenant.
+ *
+ * Previously, with Symfony's EventSubscriberInterface,
+ *  MediaUploadTenantDirectoryNamer was called before the
+ *  Doctrine prePersist event. With AsEventListener, this is not the case,
+ *  therefore the addition of VichUploaderBundle pre upload listener.
  */
 #[AsDoctrineListener(event: Events::prePersist)]
+#[AsEventListener(event: VichEvents::PRE_UPLOAD, method: 'vichPreUpload')]
 class PrePersistListener
 {
     public function __construct(
         private Security $security
     ) {}
 
-    public function prePersist(LifecycleEventArgs $args): void
+    public function prePersist(PrePersistEventArgs $args): void
     {
-        $this->setTenant($args);
+        $this->setTenant($args->getObject());
+    }
+
+    public function vichPreUpload(Event $event): void
+    {
+        $this->setTenant($event->getObject());
     }
 
     /**
      * Set entity tenant from users active tenant.
      *
-     * @param LifecycleEventArgs $args
+     * @param Object $object
      *
      * @return void
      */
-    private function setTenant(LifecycleEventArgs $args): void
+    private function setTenant(Object $object): void
     {
-        $entity = $args->getObject();
-
-        if (!$entity instanceof TenantScopedEntityInterface) {
+        if (!$object instanceof TenantScopedEntityInterface) {
             return;
         }
 
         $user = $this->security->getUser();
 
         if ($user instanceof User) {
-            $entity->setTenant($user->getActiveTenant());
+            $object->setTenant($user->getActiveTenant());
         }
     }
 }
