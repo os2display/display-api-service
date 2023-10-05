2f34c11080ea90727d531960eaadd6b14443b82b
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2023-10-05 11:07:05 +0200

    DISPLAY-993: Changed getUsername to providerId to fix issue with lexik_jwt_authentication

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -102,7 +102,7 @@ class User extends AbstractBaseEntity implements UserInterface, PasswordAuthenti
      */
     public function getUsername(): string
     {
-        return $this->email;
+        return $this->providerId;
     }
 
     /**
