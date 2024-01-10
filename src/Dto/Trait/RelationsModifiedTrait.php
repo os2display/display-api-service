0711de82752ca7ba62127bc15182ebd927995572
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2024-01-10 20:09:36 +0100

    #369: Added relationsModified to serialization groups

--- src/Dto/Trait/RelationsModifiedTrait.php
+++ src/Dto/Trait/RelationsModifiedTrait.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace App\Dto\Trait;
 
 use ApiPlatform\Metadata\ApiProperty;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 trait RelationsModifiedTrait
 {
     #[ApiProperty(schema: ['type' => 'object'])]
+    #[Groups(['playlist-slide:read', 'slides/playlists:read'])]
     private ?array $relationsModified;
 
     public function getRelationsModified(): ?array
