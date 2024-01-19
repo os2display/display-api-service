414792dafb62b652e7257c1aa528ab9b59aada00
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2024-01-19 11:49:37 +0100

    #394: Added comment

--- src/Dto/EmptyDTO.php
+++ src/Dto/EmptyDTO.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace App\Dto;
 
-// Used for POST endpoints that do not expect content.
+// Used for POST endpoints that expect empty content.
+// This is used to avoid having to change content type for a few endpoints in the API.
 class EmptyDTO
 {
 }
