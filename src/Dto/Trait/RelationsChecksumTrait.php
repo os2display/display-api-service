fbb7792900530033d88d59e12575388734788d90
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2024-02-23 10:45:41 +0100

    #369: Added groups to RelationsChecksumTrait

--- src/Dto/Trait/RelationsChecksumTrait.php
+++ src/Dto/Trait/RelationsChecksumTrait.php
@@ -5,10 +5,23 @@ declare(strict_types=1);
 namespace App\Dto\Trait;
 
 use ApiPlatform\Metadata\ApiProperty;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 trait RelationsChecksumTrait
 {
     #[ApiProperty(schema: ['type' => 'object'])]
+    #[Groups([
+        'campaigns/screen-groups:read',
+        'campaigns/screens:read',
+        'playlist-screen-region:read',
+        'playlist-slide:read',
+        'read',
+        'slides/playlists:read',
+        'screen-campaigns:read',
+        'screen-groups/campaigns:read',
+        'screen-groups/screens:read',
+        'screens/screen-groups:read',
+    ])]
     private ?array $relationsChecksum;
 
     public function getRelationsChecksum(): ?array
