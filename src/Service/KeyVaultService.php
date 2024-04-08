f7ff802817edb8832f8950cc23ba7ceb8a1849e9
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2024-04-08 12:28:20 +0200

    964: Removed azure key vault references

--- src/Service/KeyVaultService.php
+++ src/Service/KeyVaultService.php
@@ -8,7 +8,6 @@ readonly class KeyVaultService
 {
     // APP_KEY_VAULT_SOURCE (set in environment/.env) options:
     public const ENVIRONMENT = 'ENVIRONMENT';
-    public const AZURE_KEY_VAULT = 'AZURE_KEY_VAULT';
 
     public function __construct(
         private string $keyVaultSource,
@@ -24,20 +23,11 @@ readonly class KeyVaultService
     {
         return match ($this->keyVaultSource) {
             self::ENVIRONMENT => $this->getValueFromEnvironment($key),
-            self::AZURE_KEY_VAULT => $this->getValueFromAzureKeyVault($key),
         };
     }
 
     private function getValueFromEnvironment(string $key): ?string
     {
         return $this->keyVaultArray[$key] ?? null;
-    }
-
-    private function getValueFromAzureKeyVault(string $key): ?string
-    {
-        // TODO: Add support for Azure KeyVault.
-        // https://github.com/itk-dev/AzureKeyVaultPhp
-
-        throw new \Exception('Not implemented');
     }
 }
