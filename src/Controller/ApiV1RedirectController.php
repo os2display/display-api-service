4b388b0adf0dfc8c426f612ea89d6323d6a96a79
turegjorup <ture@ignatz.dk>
2024-04-09 12:29:18 +0200

    Add /v1 - /v2 redirect controller

---
+++ src/Controller/ApiV1RedirectController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ApiV1RedirectController extends AbstractController
+{
+    #[Route('/v1/{endpoint}', name: 'app_api_v1_redirect', requirements: ['endpoint' => '.+'], defaults: ['endpoint' => null], methods: ['GET'])]
+    public function index(string $endpoint): RedirectResponse
+    {
+        return $this->redirect('/v2'.$endpoint, 301);
+    }
+}
