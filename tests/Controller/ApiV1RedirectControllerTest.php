d65299a1fa48ac69114f75a21f3fbda708ed7966
turegjorup <ture@ignatz.dk>
2024-04-09 12:55:24 +0200

    1137: Use authenticated client in test

--- tests/Controller/ApiV1RedirectControllerTest.php
+++ tests/Controller/ApiV1RedirectControllerTest.php
@@ -4,18 +4,15 @@ declare(strict_types=1);
 
 namespace App\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Tests\AbstractBaseApiTestCase;
 
-class ApiV1RedirectControllerTest extends WebTestCase
+class ApiV1RedirectControllerTest extends AbstractBaseApiTestCase
 {
     public function testIndex()
     {
-        $client = static::createClient();
+        $client = $this->getAuthenticatedClient('ROLE_ADMIN');
         $crawler = $client->request('GET', '/v1/screens/01GN9PW2Z03V8VQG7SN6Q9R17H');
 
         $this->assertResponseRedirects('/v2/screens/01GN9PW2Z03V8VQG7SN6Q9R17H', 301);
-
-        $client->followRedirect();
-        $this->assertResponseIsSuccessful();
     }
 }
