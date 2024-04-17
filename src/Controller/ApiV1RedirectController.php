2e4cbb7ae58132af6977c1afeb033b9758aae48e
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2024-04-17 11:31:00 +0200

    1190: Merged routes

--- src/Controller/ApiV1RedirectController.php
+++ src/Controller/ApiV1RedirectController.php
@@ -11,15 +11,9 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class ApiV1RedirectController extends AbstractController
 {
-    #[Route('/v1/{endpoint}', name: 'app_api_v1_redirect_get', requirements: ['endpoint' => '.+'], defaults: ['endpoint' => null], methods: ['GET'])]
-    public function redirectGets(string $endpoint): RedirectResponse
+    #[Route('/v1/{endpoint}', name: 'app_api_v1_redirect', requirements: ['endpoint' => '.+'], defaults: ['endpoint' => null], methods: ['GET', 'POST'])]
+    public function index(string $endpoint): RedirectResponse
     {
         return $this->redirect('/v2/'.$endpoint, Response::HTTP_MOVED_PERMANENTLY);
-    }
-
-    #[Route('/v1/{endpoint}', name: 'app_api_v1_redirect_post', requirements: ['endpoint' => '.+'], defaults: ['endpoint' => null], methods: ['POST'])]
-    public function redirectPosts(string $endpoint): RedirectResponse
-    {
-        return $this->redirect('/v2/'.$endpoint, Response::HTTP_TEMPORARY_REDIRECT);
     }
 }
