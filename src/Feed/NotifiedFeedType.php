ed8330d3f2a90e67ac9ce5deb0999bd37f540073
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2024-04-28 07:23:28 +0200

    1203: Started work on Notified feed

--- src/Feed/NotifiedFeedType.php
+++ src/Feed/NotifiedFeedType.php
@@ -7,28 +7,24 @@ namespace App\Feed;
 use App\Entity\Tenant\Feed;
 use App\Entity\Tenant\FeedSource;
 use App\Service\FeedService;
-use Psr\Cache\CacheItemInterface;
-use Psr\Cache\InvalidArgumentException;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Uid\Ulid;
-use Symfony\Contracts\Cache\CacheInterface;
-use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
-/** @deprecated The SparkleIO service is discontinued.  */
-class SparkleIOFeedType implements FeedTypeInterface
+/**
+ * @see https://api.listen.notified.com/docs/index.html
+ */
+class NotifiedFeedType implements FeedTypeInterface
 {
     final public const SUPPORTED_FEED_TYPE = 'instagram';
     final public const REQUEST_TIMEOUT = 10;
 
+    private const BASE_URL = 'https://api.listen.notified.com';
+
     public function __construct(
         private readonly FeedService $feedService,
         private readonly HttpClientInterface $client,
-        private readonly CacheInterface $feedsCache,
         private readonly LoggerInterface $logger
     ) {}
 
@@ -41,7 +37,7 @@ class SparkleIOFeedType implements FeedTypeInterface
     {
         try {
             $secrets = $feed->getFeedSource()?->getSecrets();
-            if (!isset($secrets['baseUrl']) || !isset($secrets['clientId']) || !isset($secrets['clientSecret'])) {
+            if (!isset($secrets['token'])) {
                 return [];
             }
 
@@ -50,19 +46,28 @@ class SparkleIOFeedType implements FeedTypeInterface
                 return [];
             }
 
-            $baseUrl = $secrets['baseUrl'];
-            $clientId = $secrets['clientId'];
-            $clientSecret = $secrets['clientSecret'];
-            $token = $this->getToken($baseUrl, $clientId, $clientSecret);
+            $token = $secrets['token'];
 
             $res = $this->client->request(
-                'GET',
-                $baseUrl.'v0.1/feed/'.$configuration['feeds'][0],
+                'POST',
+                self::BASE_URL.'/api/listen/mentions',
                 [
                     'timeout' => self::REQUEST_TIMEOUT,
                     'headers' => [
-                        'Authorization' => sprintf('Bearer %s', $token),
+                        "Accept" => 'application/json',
+                        "Content-Type" => 'application/json',
+                        'Notified-Custom-Token' => $token,
                     ],
+                    'body' => json_encode([
+                        "pageSize" => 10,
+                        "page" => 1,
+                        "mediaTypes" => [],
+                        "searchProfileIds" => $configuration['feeds'],
+                        "tagIds" => [],
+                        "from" => "2023-06-24T11:23:30.294Z",
+                        "to" => "2024-04-30T11:23:30.294Z",
+                        "sourceIds" => [],
+                    ])
                 ]
             );
 
@@ -70,7 +75,7 @@ class SparkleIOFeedType implements FeedTypeInterface
             $data = json_decode($contents, false, 512, JSON_THROW_ON_ERROR);
 
             $res = [];
-            foreach ($data->items as $item) {
+            foreach ($data as $item) {
                 $res[] = $this->getFeedItemObject($item);
             }
 
@@ -95,7 +100,7 @@ class SparkleIOFeedType implements FeedTypeInterface
         // @TODO: Translation.
         return [
             [
-                'key' => 'sparkle-io-selector',
+                'key' => 'notified-selector',
                 'input' => 'multiselect-from-endpoint',
                 'endpoint' => $endpoint,
                 'name' => 'feeds',
@@ -115,23 +120,21 @@ class SparkleIOFeedType implements FeedTypeInterface
             if ('feeds' === $name) {
                 $secrets = $feedSource->getSecrets();
 
-                if (!isset($secrets['baseUrl']) || !isset($secrets['clientId']) || !isset($secrets['clientSecret'])) {
+                if (!isset($secrets['token'])) {
                     return [];
                 }
 
-                $baseUrl = $secrets['baseUrl'];
-                $clientId = $secrets['clientId'];
-                $clientSecret = $secrets['clientSecret'];
-
-                $token = $this->getToken($baseUrl, $clientId, $clientSecret);
+                $token = $secrets['token'];
 
                 $response = $this->client->request(
                     'GET',
-                    $baseUrl.'v0.1/feed',
+                    self::BASE_URL.'/api/listen/searchprofiles',
                     [
                         'timeout' => self::REQUEST_TIMEOUT,
                         'headers' => [
-                            'Authorization' => sprintf('Bearer %s', $token),
+                            "Accept" => 'application/json',
+                            "Content-Type" => 'application/json',
+                            'Notified-Custom-Token' => $token,
                         ],
                     ]
                 );
@@ -167,7 +170,7 @@ class SparkleIOFeedType implements FeedTypeInterface
      */
     public function getRequiredSecrets(): array
     {
-        return ['baseUrl', 'clientId', 'clientSecret'];
+        return ['token'];
     }
 
     /**
@@ -187,55 +190,6 @@ class SparkleIOFeedType implements FeedTypeInterface
     }
 
     /**
-     * Get oAuth token.
-     *
-     * @return string
-     *
-     * @throws RedirectionExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws TransportExceptionInterface
-     * @throws ClientExceptionInterface
-     * @throws \JsonException|InvalidArgumentException
-     */
-    private function getToken(string $baseUrl, string $clientId, string $clientSecret): string
-    {
-        /** @var CacheItemInterface $cacheItem */
-        $cacheItem = $this->feedsCache->getItem('sparkleio-token');
-
-        if ($cacheItem->isHit()) {
-            /** @var string $token */
-            $token = $cacheItem->get();
-        } else {
-            $response = $this->client->request(
-                'POST',
-                $baseUrl.'oauth/token',
-                [
-                    'timeout' => self::REQUEST_TIMEOUT,
-                    'headers' => [
-                        'Content-Type' => 'application/x-www-form-urlencoded',
-                    ],
-                    'body' => [
-                        'grant_type' => urlencode('client_credentials'),
-                        'client_id' => urlencode($clientId),
-                        'client_secret' => urlencode($clientSecret),
-                    ],
-                ]
-            );
-
-            $content = $response->getContent();
-            $contentDecoded = json_decode($content, false, 512, JSON_THROW_ON_ERROR);
-
-            $token = $contentDecoded->access_token;
-            $expireSeconds = intval($contentDecoded->expires_in / 1000 - 30);
-
-            $cacheItem->set($token);
-            $cacheItem->expiresAfter($expireSeconds);
-        }
-
-        return $token;
-    }
-
-    /**
      * Parse feed item into object.
      *
      * @return object
@@ -243,12 +197,12 @@ class SparkleIOFeedType implements FeedTypeInterface
     private function getFeedItemObject(object $item): object
     {
         return (object) [
-            'text' => $item->text,
-            'textMarkup' => null !== $item->text ? $this->wrapTags($item->text) : null,
-            'mediaUrl' => $item->mediaUrl,
-            'videoUrl' => $item->videoUrl,
-            'username' => $item->username,
-            'createdTime' => $item->createdTime,
+            'text' => $item->description ?? null,
+            'textMarkup' => null !== $item->description ? $this->wrapTags($item->description) : null,
+            'mediaUrl' => $item->mediaUrl ?? null,
+            'videoUrl' => $item->videoUrl ?? null,
+            'username' => $item->sourceName ?? null,
+            'createdTime' => $item->published ?? null,
         ];
     }
 
