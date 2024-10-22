c5a33ab51280402b01ca3aa9e90859bc38c673cd
Sine Jespersen <sinejespersen@gmail.com>
2024-10-22 20:58:53 +0200

    2314: add phpdoc_to_comment false to php-cs-fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -16,6 +16,7 @@ return (new PhpCsFixer\Config())
         'phpdoc_align' => false,
         'no_superfluous_phpdoc_tags' => false,
         'array_syntax' => ['syntax' => 'short'],
+        'phpdoc_to_comment' => false,
         'declare_strict_types' => true,
         ConstructorEmptyBracesFixer::name() => true,
         MultilinePromotedPropertiesFixer::name() => true,
