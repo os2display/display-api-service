48e7ed1e836b70963f671997c2c34301d9f0d367
Jeppe Krogh <kjej@aarhus.dk>
2024-11-11 16:15:17 +0100

    Added getSchema method in RssFeedType class

--- src/Feed/RssFeedType.php
+++ src/Feed/RssFeedType.php
@@ -136,4 +136,16 @@ class RssFeedType implements FeedTypeInterface
     {
         return self::SUPPORTED_FEED_TYPE;
     }
+
+    public static function getSchema(): mixed
+    {
+        $jsonSchema = <<<'JSON'
+        {
+          "$schema": "http://json-schema.org/draft-04/schema#",
+          "type": "object"
+        }
+        JSON;
+
+        return json_decode($jsonSchema, false, 512, JSON_THROW_ON_ERROR);
+    }
 }
