cc27bdeb9b393643adb6dd7ea873f1cbb042f24b
Jeppe Krogh <kjej@aarhus.dk>
2024-11-12 09:53:20 +0100

    Added dynamic getSchema method in SparkleIOFeedType class.

--- src/Feed/SparkleIOFeedType.php
+++ src/Feed/SparkleIOFeedType.php
@@ -284,4 +284,16 @@ class SparkleIOFeedType implements FeedTypeInterface
 
         return $text;
     }
+
+    public function getSchema(): mixed
+    {
+        $jsonSchema = <<<'JSON'
+        {
+          "$schema": "http://json-schema.org/draft-04/schema#",
+          "type": "object"
+        }
+        JSON;
+
+        return json_decode($jsonSchema, false, 512, JSON_THROW_ON_ERROR);
+    }
 }
