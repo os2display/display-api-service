7d8b35d7e34a56bb329777c89edba4534613d020
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2025-07-01 08:58:20 +0200

    4565: Added rules to php cs fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,6 +4,9 @@
 
 // https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/blob/master/doc/config.rst
 
+use PhpCsFixerCustomFixers\Fixer\ConstructorEmptyBracesFixer;
+use PhpCsFixerCustomFixers\Fixer\MultilinePromotedPropertiesFixer;
+
 $finder = new PhpCsFixer\Finder();
 // Check all files …
 $finder->in(__DIR__);
@@ -13,8 +16,18 @@ $finder->ignoreVCSIgnored(true);
 $config = new PhpCsFixer\Config();
 $config->setFinder($finder);
 
+$config->registerCustomFixers(new PhpCsFixerCustomFixers\Fixers());
+$config->setRiskyAllowed(true);
+
 $config->setRules([
-  '@Symfony' => true,
+    '@Symfony' => true,
+    'phpdoc_align' => false,
+    'no_superfluous_phpdoc_tags' => false,
+    'array_syntax' => ['syntax' => 'short'],
+    'phpdoc_to_comment' => false,
+    'declare_strict_types' => true,
+    ConstructorEmptyBracesFixer::name() => true,
+    MultilinePromotedPropertiesFixer::name() => true,
 ]);
 
 return $config;
