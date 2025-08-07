31beaa967b88e97bbfe6402e5bfac069a4a5cd7b
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2025-08-07 08:35:51 +0200

    4565: Reverted to use swc instead of oxc

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import symfonyPlugin from "vite-plugin-symfony";
-import react from "@vitejs/plugin-react-oxc";
+import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
+
+// TODO: When possible change to @vitejs/plugin-react-oxc.
 
 export default defineConfig(({ command }) => {
   return {
