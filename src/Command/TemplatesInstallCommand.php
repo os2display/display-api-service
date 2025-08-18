633b22727bcc61822e8daa141a1edc5f771d55ee
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2025-08-18 12:58:05 +0200

    4565: Cleanup

--- src/Command/TemplatesInstallCommand.php
+++ src/Command/TemplatesInstallCommand.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace App\Command;
 
-use App\Entity\Template;
 use App\Service\TemplateService;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Id\AssignedGenerator;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -15,7 +13,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\Uid\Ulid;
 
 #[AsCommand(
     name: 'app:templates:install',
@@ -24,7 +21,7 @@ use Symfony\Component\Uid\Ulid;
 class TemplatesInstallCommand extends Command
 {
     public function __construct(
-        private readonly TemplateService $templateService, private readonly EntityManagerInterface $entityManager,
+        private readonly TemplateService $templateService,
     ) {
         parent::__construct();
     }
