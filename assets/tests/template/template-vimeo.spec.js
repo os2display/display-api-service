f8f706b07274eb304700e269507c47569e651e37
Sine Jespersen <sinejespersen@gmail.com>
2025-09-01 11:17:44 +0200

    5317: add vimeo test

--- assets/tests/template/template-vimeo.spec.js
+++ assets/tests/template/template-vimeo.spec.js
@@ -1,7 +1,12 @@
 import { test, expect } from "@playwright/test";
 
 test("Vimeo Player 0", async ({ page }) => {
-  await page.goto("/template/vimeo-player-0");
+  await page.goto("template/vimeo-player-0");
 
-  // TODO
+  const iframe = page.locator("iframe").first();
+  await expect(iframe).toBeVisible();
+  await expect(iframe).toHaveAttribute(
+    "src",
+    "https://player.vimeo.com/video/882393277?muted=1&autoplay=1&pip=0&controls=0&loop=1&quality=undefined&app_id=122963&texttrack=undefined",
+  );
 });
