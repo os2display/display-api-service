54673b6a2b8988bbcde7f84ea08b5dfece18538f
sinejespersen <sinejespersen@protonmail.com>
2025-09-12 12:27:43 +0200

    5317: iframe template test

--- assets/tests/template/template-iframe.spec.js
+++ assets/tests/template/template-iframe.spec.js
@@ -1,7 +1,11 @@
 import { test, expect } from "@playwright/test";
 
-test("IFrame 0", async ({ page }) => {
+test("iframe-0: ui tests", async ({ page }) => {
   await page.goto("/template/iframe-0");
-
-  // TODO
+  const iframe = page.locator("iframe");
+  await expect(iframe).toBeVisible();
+  await expect(iframe).toHaveAttribute(
+    "src",
+    "https://images.unsplash.com/photo-1551373884-8a0750074df7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2370&q=80",
+  );
 });
