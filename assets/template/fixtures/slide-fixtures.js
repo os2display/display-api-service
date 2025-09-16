9883a36106dfbe76cd43c2e94d5ae119a81ff628
sinejespersen <sinejespersen@protonmail.com>
2025-09-16 13:36:41 +0200

    5317: change url in iframe

--- assets/template/fixtures/slide-fixtures.js
+++ assets/template/fixtures/slide-fixtures.js
@@ -772,8 +772,7 @@ const slideFixtures = [
     },
     content: {
       duration: 5000,
-      source:
-        "https://images.unsplash.com/photo-1551373884-8a0750074df7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2370&q=80",
+      source: "https://display.local.itkdev.dk/docs",
     },
   },
   {
--- assets/tests/template/template-iframe.spec.js
+++ assets/tests/template/template-iframe.spec.js
@@ -6,6 +6,6 @@ test("iframe-0: ui tests", async ({ page }) => {
   await expect(iframe).toBeVisible();
   await expect(iframe).toHaveAttribute(
     "src",
-    "https://images.unsplash.com/photo-1551373884-8a0750074df7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2370&q=80",
+    "https://display.local.itkdev.dk/docs",
   );
 });
