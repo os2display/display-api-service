07577348d3a724e96acbf2f56d710061f556db3c
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2025-12-20 12:45:06 +0100

    6237: Added tiptap as rich text editor

--- assets/admin/components/util/forms/rich-text/rich-text.jsx
+++ assets/admin/components/util/forms/rich-text/rich-text.jsx
@@ -1,6 +1,164 @@
 import { FormGroup, FormLabel } from "react-bootstrap";
 import DOMPurify from "dompurify";
-import "./rich-text.scss";
+//import "./rich-text.scss";
+import { EditorContent, useEditor, useEditorState } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { FloatingMenu } from "@tiptap/react/menus";
+
+const extensions = [StarterKit]
+
+function MenuBar({ editor }) {
+  const editorState = useEditorState({
+    editor,
+    selector: ctx => {
+      return {
+        isBold: ctx.editor.isActive('bold') ?? false,
+        canBold: ctx.editor.can().chain().toggleBold().run() ?? false,
+        isItalic: ctx.editor.isActive('italic') ?? false,
+        canItalic: ctx.editor.can().chain().toggleItalic().run() ?? false,
+        isStrike: ctx.editor.isActive('strike') ?? false,
+        canStrike: ctx.editor.can().chain().toggleStrike().run() ?? false,
+        isParagraph: ctx.editor.isActive('paragraph') ?? false,
+        isHeading1: ctx.editor.isActive('heading', { level: 1 }) ?? false,
+        isHeading2: ctx.editor.isActive('heading', { level: 2 }) ?? false,
+        isHeading3: ctx.editor.isActive('heading', { level: 3 }) ?? false,
+        isHeading4: ctx.editor.isActive('heading', { level: 4 }) ?? false,
+        isHeading5: ctx.editor.isActive('heading', { level: 5 }) ?? false,
+        isHeading6: ctx.editor.isActive('heading', { level: 6 }) ?? false,
+        isBulletList: ctx.editor.isActive('bulletList') ?? false,
+        isOrderedList: ctx.editor.isActive('orderedList') ?? false,
+        isBlockquote: ctx.editor.isActive('blockquote') ?? false,
+        canUndo: ctx.editor.can().chain().undo().run() ?? false,
+        canRedo: ctx.editor.can().chain().redo().run() ?? false,
+      }
+    },
+  })
+
+  return (
+    <div className="control-group">
+      <div className="button-group">
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          disabled={!editorState.canBold}
+          className={editorState.isBold ? 'is-active' : ''}
+        >
+          Bold
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          disabled={!editorState.canItalic}
+          className={editorState.isItalic ? 'is-active' : ''}
+        >
+          Italic
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          disabled={!editorState.canStrike}
+          className={editorState.isStrike ? 'is-active' : ''}
+        >
+          Strike
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().setParagraph().run()}
+          className={editorState.isParagraph ? 'is-active' : ''}
+        >
+          Paragraph
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+          className={editorState.isHeading1 ? 'is-active' : ''}
+        >
+          H1
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+          className={editorState.isHeading2 ? 'is-active' : ''}
+        >
+          H2
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+          className={editorState.isHeading3 ? 'is-active' : ''}
+        >
+          H3
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+          className={editorState.isHeading4 ? 'is-active' : ''}
+        >
+          H4
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
+          className={editorState.isHeading5 ? 'is-active' : ''}
+        >
+          H5
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
+          className={editorState.isHeading6 ? 'is-active' : ''}
+        >
+          H6
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={editorState.isBulletList ? 'is-active' : ''}
+        >
+          Bullet list
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={editorState.isOrderedList ? 'is-active' : ''}
+        >
+          Ordered list
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleBlockquote().run()}
+          className={editorState.isBlockquote ? 'is-active' : ''}
+        >
+          Blockquote
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().setHorizontalRule().run()}
+        >
+          Horizontal rule
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().setHardBreak().run()}
+        >
+          Hard break
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().undo().run()} disabled={!editorState.canUndo}
+        >
+          Undo
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().redo().run()} disabled={!editorState.canRedo}
+        >
+          Redo
+        </button>
+      </div>
+    </div>
+  )
+}
 
 /**
  * A rich text field for forms.
@@ -35,6 +193,14 @@ function RichText({
     onChange({ target: returnTarget });
   };
 
+  const editor = useEditor({
+    extensions: extensions,
+    content: value,
+    onUpdate({editor}) {
+      onRichTextChange(editor.getHTML())
+    },
+  })
+
   return (
     <div className="text-editor">
       <FormGroup className={formGroupClasses}>
@@ -43,8 +209,10 @@ function RichText({
           {required && " *"}
         </FormLabel>
 
-        <div>Rich text</div>
-
+        <div style={{border: "1px solid #ccc", padding: "10px"}}>
+          <MenuBar editor={editor} />
+          <EditorContent editor={editor} />
+        </div>
       </FormGroup>
       {helpText && <small>{helpText}</small>}
     </div>
