aebb4a21f16cfe92a82b58ebdaeef0ad15c691d7
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2026-01-02 12:31:18 +0100

    6237: Configured and styled editor

---
+++ assets/admin/components/util/forms/rich-text/rich-text-menu.jsx
@@ -0,0 +1,140 @@
+import { useEditorState } from "@tiptap/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBold, faItalic, faLevelDownAlt, faListOl, faListUl, faParagraph, faRedo, faStrikethrough, faUndo
+} from "@fortawesome/free-solid-svg-icons";
+import { useTranslation } from "react-i18next";
+
+function RichTextMenu({ editor }) {
+  const { t } = useTranslation("common", { keyPrefix: "rich-text-editor" });
+  const editorState = useEditorState({
+    editor,
+    selector: ctx => {
+      return {
+        isBold: ctx.editor.isActive("bold") ?? false,
+        canBold: ctx.editor.can().chain().toggleBold().run() ?? false,
+        isItalic: ctx.editor.isActive("italic") ?? false,
+        canItalic: ctx.editor.can().chain().toggleItalic().run() ?? false,
+        isStrike: ctx.editor.isActive("strike") ?? false,
+        canStrike: ctx.editor.can().chain().toggleStrike().run() ?? false,
+        isHeading1: ctx.editor.isActive("heading", { level: 1 }) ?? false,
+        isHeading2: ctx.editor.isActive("heading", { level: 2 }) ?? false,
+        isHeading3: ctx.editor.isActive("heading", { level: 3 }) ?? false,
+        isHeading4: ctx.editor.isActive("heading", { level: 4 }) ?? false,
+        isBulletList: ctx.editor.isActive("bulletList") ?? false,
+        isOrderedList: ctx.editor.isActive("orderedList") ?? false,
+        isParagraph: ctx.editor.isActive('paragraph') ?? false,
+        canUndo: ctx.editor.can().chain().undo().run() ?? false,
+        canRedo: ctx.editor.can().chain().redo().run() ?? false
+      };
+    }
+  });
+
+  return (
+    <div className="control-group">
+      <div className="button-group">
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          disabled={!editorState.canBold}
+          className={editorState.isBold ? "is-active" : ""}
+          aria-label={t("toggle-bold")}
+        >
+          <FontAwesomeIcon icon={faBold} />
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          disabled={!editorState.canItalic}
+          className={editorState.isItalic ? "is-active" : ""}
+          aria-label={t("toggle-italic")}
+        >
+          <FontAwesomeIcon icon={faItalic} />
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          disabled={!editorState.canStrike}
+          className={editorState.isStrike ? "is-active" : ""}
+          aria-label={t("toggle-strike-through")}
+        >
+          <FontAwesomeIcon icon={faStrikethrough} />
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+          className={editorState.isHeading1 ? "is-active" : ""}
+          aria-label={t("toggle-heading-1")}
+        >
+          H1
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+          className={editorState.isHeading2 ? "is-active" : ""}
+          aria-label={t("toggle-heading-2")}
+        >
+          H2
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+          className={editorState.isHeading3 ? "is-active" : ""}
+          aria-label={t("toggle-heading-3")}
+        >
+          H3
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+          className={editorState.isHeading4 ? "is-active" : ""}
+          aria-label={t("toggle-heading-4")}
+        >
+          H4
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={editorState.isBulletList ? "is-active" : ""}
+          aria-label={t("toggle-bullet-list")}
+        >
+          <FontAwesomeIcon icon={faListUl} />
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={editorState.isOrderedList ? "is-active" : ""}
+          aria-label={t("toggle-ordered-list")}
+        >
+          <FontAwesomeIcon icon={faListOl} />
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().setHardBreak().run()}
+          aria-label={t("insert-hard-break")}
+        >
+          <FontAwesomeIcon icon={faLevelDownAlt} />
+        </button>
+
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().undo().run()}
+          disabled={!editorState.canUndo}
+          className="ms-3"
+          aria-label={t("undo")}
+        >
+          <FontAwesomeIcon icon={faUndo} />
+        </button>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().redo().run()} disabled={!editorState.canRedo}
+          aria-label={t("redo")}
+        >
+          <FontAwesomeIcon icon={faRedo} />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default RichTextMenu;
--- assets/admin/components/util/forms/rich-text/rich-text.jsx
+++ assets/admin/components/util/forms/rich-text/rich-text.jsx
@@ -1,164 +1,19 @@
 import { FormGroup, FormLabel } from "react-bootstrap";
 import DOMPurify from "dompurify";
-//import "./rich-text.scss";
-import { EditorContent, useEditor, useEditorState } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
-import { FloatingMenu } from "@tiptap/react/menus";
-
-const extensions = [StarterKit]
-
-function MenuBar({ editor }) {
-  const editorState = useEditorState({
-    editor,
-    selector: ctx => {
-      return {
-        isBold: ctx.editor.isActive('bold') ?? false,
-        canBold: ctx.editor.can().chain().toggleBold().run() ?? false,
-        isItalic: ctx.editor.isActive('italic') ?? false,
-        canItalic: ctx.editor.can().chain().toggleItalic().run() ?? false,
-        isStrike: ctx.editor.isActive('strike') ?? false,
-        canStrike: ctx.editor.can().chain().toggleStrike().run() ?? false,
-        isParagraph: ctx.editor.isActive('paragraph') ?? false,
-        isHeading1: ctx.editor.isActive('heading', { level: 1 }) ?? false,
-        isHeading2: ctx.editor.isActive('heading', { level: 2 }) ?? false,
-        isHeading3: ctx.editor.isActive('heading', { level: 3 }) ?? false,
-        isHeading4: ctx.editor.isActive('heading', { level: 4 }) ?? false,
-        isHeading5: ctx.editor.isActive('heading', { level: 5 }) ?? false,
-        isHeading6: ctx.editor.isActive('heading', { level: 6 }) ?? false,
-        isBulletList: ctx.editor.isActive('bulletList') ?? false,
-        isOrderedList: ctx.editor.isActive('orderedList') ?? false,
-        isBlockquote: ctx.editor.isActive('blockquote') ?? false,
-        canUndo: ctx.editor.can().chain().undo().run() ?? false,
-        canRedo: ctx.editor.can().chain().redo().run() ?? false,
-      }
-    },
-  })
-
-  return (
-    <div className="control-group">
-      <div className="button-group">
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleBold().run()}
-          disabled={!editorState.canBold}
-          className={editorState.isBold ? 'is-active' : ''}
-        >
-          Bold
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleItalic().run()}
-          disabled={!editorState.canItalic}
-          className={editorState.isItalic ? 'is-active' : ''}
-        >
-          Italic
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleStrike().run()}
-          disabled={!editorState.canStrike}
-          className={editorState.isStrike ? 'is-active' : ''}
-        >
-          Strike
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().setParagraph().run()}
-          className={editorState.isParagraph ? 'is-active' : ''}
-        >
-          Paragraph
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-          className={editorState.isHeading1 ? 'is-active' : ''}
-        >
-          H1
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-          className={editorState.isHeading2 ? 'is-active' : ''}
-        >
-          H2
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-          className={editorState.isHeading3 ? 'is-active' : ''}
-        >
-          H3
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
-          className={editorState.isHeading4 ? 'is-active' : ''}
-        >
-          H4
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
-          className={editorState.isHeading5 ? 'is-active' : ''}
-        >
-          H5
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
-          className={editorState.isHeading6 ? 'is-active' : ''}
-        >
-          H6
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleBulletList().run()}
-          className={editorState.isBulletList ? 'is-active' : ''}
-        >
-          Bullet list
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleOrderedList().run()}
-          className={editorState.isOrderedList ? 'is-active' : ''}
-        >
-          Ordered list
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().toggleBlockquote().run()}
-          className={editorState.isBlockquote ? 'is-active' : ''}
-        >
-          Blockquote
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().setHorizontalRule().run()}
-        >
-          Horizontal rule
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().setHardBreak().run()}
-        >
-          Hard break
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().undo().run()} disabled={!editorState.canUndo}
-        >
-          Undo
-        </button>
-        <button
-          type="button"
-          onClick={() => editor.chain().focus().redo().run()} disabled={!editorState.canRedo}
-        >
-          Redo
-        </button>
-      </div>
-    </div>
-  )
-}
+import { EditorContent, useEditor } from "@tiptap/react";
+import RichTextMenu from "./rich-text-menu.jsx";
+import "./rich-text.scss";
+import { BulletList, ListItem, OrderedList } from '@tiptap/extension-list';
+import Bold from '@tiptap/extension-bold';
+import Italic from '@tiptap/extension-italic';
+import Strike from '@tiptap/extension-strike';
+import Underline from '@tiptap/extension-underline';
+import Document from '@tiptap/extension-document';
+import HardBreak from '@tiptap/extension-hard-break';
+import Paragraph from '@tiptap/extension-paragraph';
+import Text from '@tiptap/extension-text';
+import { UndoRedo } from '@tiptap/extensions';
+import Heading from "@tiptap/extension-heading";
 
 /**
  * A rich text field for forms.
@@ -194,7 +49,25 @@ function RichText({
   };
 
   const editor = useEditor({
-    extensions: extensions,
+    // @see https://tiptap.dev/docs/editor/extensions/overview
+    extensions: [
+      Document,
+      Text,
+      Bold,
+      Italic,
+      Strike,
+      Heading.configure({
+        levels: [1, 2, 3, 4],
+      }),
+      Underline,
+      BulletList,
+      OrderedList,
+      ListItem,
+      Paragraph,
+      UndoRedo,
+      HardBreak,
+    ],
+    enableInputRules: false,
     content: value,
     onUpdate({editor}) {
       onRichTextChange(editor.getHTML())
@@ -204,13 +77,13 @@ function RichText({
   return (
     <div className="text-editor">
       <FormGroup className={formGroupClasses}>
-        <FormLabel htmlFor={name}>
+        <FormLabel>
           {label}
           {required && " *"}
         </FormLabel>
 
-        <div style={{border: "1px solid #ccc", padding: "10px"}}>
-          <MenuBar editor={editor} />
+        <div className="rich-text-editor">
+          <RichTextMenu editor={editor} />
           <EditorContent editor={editor} />
         </div>
       </FormGroup>
