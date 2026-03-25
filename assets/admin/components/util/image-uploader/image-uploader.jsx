dbae8dff6cc8b55e8be1a00eb446d46532d0e321
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2026-03-25 13:55:36 +0100

    6521: Removed use of abandoned react-images-uploading

--- assets/admin/components/util/image-uploader/image-uploader.jsx
+++ assets/admin/components/util/image-uploader/image-uploader.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import ImageUploading from "react-images-uploading";
+import { useEffect, useState, useCallback } from "react";
+import { useDropzone } from "react-dropzone";
 import { Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faImage } from "@fortawesome/free-solid-svg-icons";
@@ -11,7 +11,7 @@ import "./image-uploader.scss";
 
 /**
  * @param {object} props The props.
- * @param {object} props.inputImage The image object.
+ * @param {array} props.inputImage The image object.
  * @param {Function} props.handleImageUpload Callback for image upload.
  * @param {string} props.name The name of the image field.
  * @param {boolean} props.multipleImages Whether the user should be able to
@@ -74,102 +74,108 @@ function ImageUploader({
     }
   }, [inputImage]);
 
-  const onChange = (imageList) => {
-    // data for submit
-    const uniqueImages = [
-      ...new Map(imageList.map((item) => [item.url, item])).values(),
-    ];
-    setImages(uniqueImages);
-    const target = { value: imageList, id: name };
+  const onDrop = useCallback(
+    (acceptedFiles) => {
+      const newImages = acceptedFiles.map((file) => ({
+        file,
+        url: URL.createObjectURL(file),
+        title: "",
+        description: "",
+        license: "",
+      }));
+
+      const updatedImages = multipleImages
+        ? [...images, ...newImages]
+        : newImages;
+
+      const uniqueImages = [
+        ...new Map(updatedImages.map((item) => [item.url, item])).values(),
+      ];
+
+      setImages(uniqueImages);
+      const target = { value: uniqueImages, id: name };
+      handleImageUpload({ target });
+    },
+    [images, multipleImages, name, handleImageUpload]
+  );
+
+  const onImageRemove = (index) => {
+    const updatedImages = images.filter((_, i) => i !== index);
+    setImages(updatedImages);
+    const target = { value: updatedImages, id: name };
     handleImageUpload({ target });
+    setSelected([]);
   };
+
+  const { getRootProps, getInputProps, isDragActive, open } = useDropzone({
+    onDrop,
+    accept: {
+      "image/*": [".png", ".jpg", ".jpeg", ".gif", ".svg", ".webp"],
+    },
+    multiple: multipleImages,
+    noClick: true,
+    noKeyboard: true,
+  });
 
   /* eslint-disable jsx-a11y/control-has-associated-label */
   return (
     // @TODO: error handling
     <div className={error ? "invalid" : ""}>
-      <ImageUploading
-        multiple={multipleImages ?? false}
-        value={images}
-        onChange={onChange}
-        dataURLKey="url"
-      >
-        {({
-          imageList,
-          onImageUpload,
-          onImageUpdate,
-          onImageRemove,
-          isDragging,
-          dragProps,
-        }) => (
-          // write your building UI
-          <div className="upload__image-wrapper bg-light border p-3 pb-0 rounded">
-            {(imageList.length === 0 || multipleImages) && (
-              <>
-                <Button
-                  variant="success"
-                  onClick={onImageUpload}
-                  onDrop={dragProps.onDrop}
-                  onDragEnter={dragProps.onDragEnter}
-                  onDragLeave={dragProps.onDragLeave}
-                  onDragOver={dragProps.onDragOver}
-                  className="me-3"
-                >
-                  {!multipleImages && t("image-uploader.pick-image")}
-                  {multipleImages && t("image-uploader.pick-more-images")}
-                </Button>
-                {showLibraryButton && (
-                  <Button
-                    variant="success"
-                    onClick={() => setShowMediaModal(true)}
-                  >
-                    {t("image-uploader.media-library")}
-                  </Button>
-                )}
-                <button
-                  type="button"
-                  className={
-                    isDragging
-                      ? "drag-drop-area drag-drop-area-active"
-                      : "drag-drop-area"
-                  }
-                  onClick={onImageUpload}
-                  onDrop={dragProps.onDrop}
-                  onDragEnter={dragProps.onDragEnter}
-                  onDragLeave={dragProps.onDragLeave}
-                  onDragOver={dragProps.onDragOver}
-                >
-                  <FontAwesomeIcon icon={faImage} />
-                </button>
-
-                <small
-                  id="aria-label-for-drag-and-drop"
-                  className="form-text mb-3"
-                >
-                  {t("image-uploader.help-text")}
-                </small>
-              </>
+      <div className="upload__image-wrapper bg-light border p-3 pb-0 rounded">
+        {(images.length === 0 || multipleImages) && (
+          <>
+            <Button
+              variant="success"
+              onClick={open}
+              className="me-3"
+            >
+              {!multipleImages && t("image-uploader.pick-image")}
+              {multipleImages && t("image-uploader.pick-more-images")}
+            </Button>
+            {showLibraryButton && (
+              <Button
+                variant="success"
+                onClick={() => setShowMediaModal(true)}
+              >
+                {t("image-uploader.media-library")}
+              </Button>
             )}
-            {imageList.map((image, index) => {
-              const key = image?.file ? image.file.name : image["@id"];
-              return (
-                <Image
-                  inputImage={image}
-                  handleChange={handleChange}
-                  onImageUpdate={onImageUpdate}
-                  onImageRemove={() => {
-                    onImageRemove();
-                    setSelected([]);
-                  }}
-                  index={index}
-                  key={`image-${key}`}
-                />
-              );
-            })}
-          </div>
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              <button
+                type="button"
+                className={
+                  isDragActive
+                    ? "drag-drop-area drag-drop-area-active"
+                    : "drag-drop-area"
+                }
+                onClick={open}
+              >
+                <FontAwesomeIcon icon={faImage} />
+              </button>
+            </div>
+
+            <small
+              id="aria-label-for-drag-and-drop"
+              className="form-text mb-3"
+            >
+              {t("image-uploader.help-text")}
+            </small>
+          </>
         )}
-      </ImageUploading>
-      {/* @TODO: error handling */}
+        {images.map((image, index) => {
+          const key = image?.file ? image.file.name : image["@id"];
+          return (
+            <Image
+              inputImage={image}
+              handleChange={handleChange}
+              onImageRemove={() => onImageRemove(index)}
+              index={index}
+              key={`image-${key}`}
+            />
+          );
+        })}
+      </div>
       {error && (
         <div className="invalid-feedback-image-uploader">
           {invalidInputText}
