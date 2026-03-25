f3dadd148da64e8d73b4e35e3a81eb33f3dfca75
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2026-03-25 13:38:09 +0100

    6521: Fixed issues

--- assets/admin/components/media/image-list.jsx
+++ assets/admin/components/media/image-list.jsx
@@ -52,7 +52,7 @@ function ImageList({ media = [], multiple }) {
             <Form.Check
               type="checkbox"
               onClick={() => selectImage(data)}
-              checked={selected.find((item) => item.id === data["@id"])}
+              checked={!!selected.find((item) => item.id === data["@id"])}
               aria-label={t("checkbox-form-aria-label", { this: data.title })}
             />
             <div className="card-body">
