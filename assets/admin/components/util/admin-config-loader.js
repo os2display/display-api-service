b5f594c9d27461d595fce25ce1195fb28664bd45
turegjorup <ture@ignatz.dk>
2026-05-26 12:31:12 +0200

    7259: Cached default config in AdminConfigLoader on fetch failure
    Without this every consumer mount re-fired a failing /config/admin request.

--- assets/admin/components/util/admin-config-loader.js
+++ assets/admin/components/util/admin-config-loader.js
@@ -1,3 +1,21 @@
+const DEFAULT_CONFIG = {
+  rejseplanenApiKey: null,
+  touchButtonRegions: false,
+  showScreenStatus: false,
+  enhancedPreview: false,
+  loginScreenText: "",
+  mediaMaxUploadSizeMb: 200,
+  loginMethods: [
+    {
+      type: "username-password",
+      enabled: true,
+      provider: "username-password",
+      label: null,
+      icon: null,
+    },
+  ],
+};
+
 let configData = null;
 let activePromise = null;
 
@@ -6,49 +24,25 @@ const AdminConfigLoader = {
     if (activePromise) {
       return activePromise;
     }
+    if (configData !== null) {
+      return configData;
+    }
 
-    activePromise = new Promise((resolve) => {
-      if (configData !== null) {
-        resolve(configData);
-      } else {
-        fetch("/config/admin")
-          .then((response) => response.json())
-          .then((data) => {
-            configData = data;
-            resolve(configData);
-          })
-          .catch(() => {
-            if (configData !== null) {
-              resolve(configData);
-            } else {
-              // eslint-disable-next-line no-console
-              console.error("Could not load config. Will use default config.");
-
-              // Default config.
-              resolve({
-                rejseplanenApiKey: null,
-                touchButtonRegions: false,
-                showScreenStatus: false,
-                enhancedPreview: false,
-                loginScreenText: "",
-                mediaMaxUploadSizeMb: 200,
-                loginMethods: [
-                  {
-                    type: "username-password",
-                    enabled: true,
-                    provider: "username-password",
-                    label: null,
-                    icon: null,
-                  },
-                ],
-              });
-            }
-          })
-          .finally(() => {
-            activePromise = null;
-          });
-      }
-    });
+    activePromise = fetch("/config/admin")
+      .then((response) => response.json())
+      .then((data) => {
+        configData = data;
+        return configData;
+      })
+      .catch(() => {
+        // eslint-disable-next-line no-console
+        console.error("Could not load config. Will use default config.");
+        configData = DEFAULT_CONFIG;
+        return configData;
+      })
+      .finally(() => {
+        activePromise = null;
+      });
 
     return activePromise;
   },
